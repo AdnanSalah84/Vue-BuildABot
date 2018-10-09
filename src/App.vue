35e6d38119cdfc68336a56bd4b0490b3670f7bac
adnansalah84 <adnan.salah84@gmail.com>
2018-10-09 15:38:00 +0500

    creating global and namespace

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    <header>
+    Root Foo: {{rootFoo}}<br/>
+    Robots Foo: {{robotsFoo}}<br/>
+    Users Foo: {{usersFoo}}<br/>
+    <br/>
+    Root Getter Foo: {{rootGetterFoo}} <br/>
+    Robots Getter Foo: {{robotsGetterFoo}} <br/>
+    Users Getter Foo: {{usersGetterFoo}} <br/>
+    <header> 
       <nav>
         <ul>
           <li class="nav-item">
@@ -44,6 +51,24 @@
   export default {
     name: 'app',
     computed: {
+      rootFoo(){
+         return this.$store.state.foo;
+      },
+      robotsFoo(){
+        return this.$store.state.robots.foo;
+      },
+      usersFoo(){
+        return this.$store.state.users.foo;
+      },
+      rootGetterFoo(){
+        return this.$store.getters.foo;
+      },
+      robotsGetterFoo(){
+        return this.$store.getters['robots/foo'];
+      },
+      usersGetterFoo(){
+        return this.$store.getters['users/foo'];
+      },
       cart(){
         return this.$store.state.robots.cart;
       }
